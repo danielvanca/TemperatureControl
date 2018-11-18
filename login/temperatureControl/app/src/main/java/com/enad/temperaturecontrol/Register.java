d4d9b429b992894cb5eb889be2f31c7996ba2701
Daniel Vanca <daniel.vancad@gmail.com>
2018-11-18 18:47:51 +0200

    Android app added

---
+++ login/temperatureControl/app/src/main/java/com/enad/temperaturecontrol/Register.java
@@ -0,0 +1,29 @@
+package com.enad.temperaturecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity
+{
+    EditText username, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        username = (EditText)findViewById(R.id.editTextUsernameRegister);
+        password = (EditText)findViewById(R.id.editTextPasswordRegister);
+    }
+
+    public void OnReg(View view)
+    {
+        String str_username = username.getText().toString();
+        String str_password = password.getText().toString();
+        String type = "register";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, str_username, str_password);
+    }
+}
