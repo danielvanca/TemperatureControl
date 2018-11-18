6cdf9a192ff5207eb38a5579698c132ca077b878
Daniel Vanca <daniel.vancad@gmail.com>
2018-11-18 18:48:23 +0200

    webapp files

---
+++ webapp/register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<link rel="stylesheet" href="styles.css">
+    <title>  Register </title>
+    <meta charset="UTF-8">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+        <form action="register2.php" method="post">
+            <div class="container">
+                <h1>Register</h1>
+                <p>Please fill in this form to create an account.</p>
+                <hr>
+
+                <label for="user"><b>Username: </b></label>
+                <input type="text" placeholder="Enter username" name="user" required>
+                <br>
+                <label for="pwd"><b>Password: </b></label>
+                <input type="password" placeholder="Enter password" name="pwd" required>
+
+                <hr>
+
+   
+                 <button type="submit" class="buttonRegister">Register</button>
+            </div>
+
+            <div class="container signin">
+                 <p>Already have an account? <a href="/TemperatureControl/login/temperatureControl/login.php"> Sign in</a>.</p>
+             </div>
+        </form> 
+
+</body>
+
+</html>
+
---
+++ webapp/registerCompleted.php
@@ -0,0 +1,41 @@
+<?php
+    
+      
+    include 'config.php';
+
+    if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
+    $sql = "INSERT INTO loginregister (user, pwd) VALUES ('$_POST[user]', '$_POST[pwd]')";
+
+    if (mysqli_query($db, $sql))
+    {
+        
+    }
+    else
+    {
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
+    }
+    mysqli_close($db);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<link rel="stylesheet" href="styles.css">
+    <title>  Register completed </title>
+    <meta charset="UTF-8">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <div id="login">
+        <h1>Account created succesfully!!</h1>
+        <h2> </br> <a href=/TemperatureControl/login/temperatureControl/index.php> Home </a> </h2>
+    </div>
+</body>
+
+</html>
